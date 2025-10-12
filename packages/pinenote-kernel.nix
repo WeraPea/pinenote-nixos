d420d79647c659ca435513864e17592cf0f7b672
Weronika <sokneip@tuta.io>
2025-10-12 04:27:56 +0200

    Make pinenote kernel overridable

--- packages/pinenote-kernel.nix
+++ packages/pinenote-kernel.nix
@@ -1,25 +1,38 @@
 {
-  pkgs,
+  lib,
+  buildLinux,
   fetchFromSourcehut,
+  structuredExtraConfig ? { },
+  argsOverride ? { },
   ...
-}:
-(pkgs.buildLinux rec {
-  src = fetchFromSourcehut {
-    owner = "~hrdl";
-    repo = "linux";
-    rev = "1a119bb3028b09cab962781aa3b6992ed7a3aa1e";
-    sha256 = "sha256-UKwjXJ5CyUoGpiNkyYl/2sg7E3Iw8Lsv5/1IJkdbvAo=";
-  };
+}@args:
+let
   version = "6.17.0-rc5";
-  modDirVersion = version;
-  defconfig = "pinenote_defconfig";
-  ignoreConfigErrors = true; # from jzbor/nix-parcels
-  extraConfig = ''
-    VIDEO_THP7312 n
-    CRYPTO_AEGIS128_SIMD n
-    ROCKCHIP_DW_HDMI_QP n
-  ''; # fails to build otherwise
-}).overrideAttrs
+in
+(buildLinux (
+  args
+  // {
+    src = fetchFromSourcehut {
+      owner = "~hrdl";
+      repo = "linux";
+      rev = "1a119bb3028b09cab962781aa3b6992ed7a3aa1e";
+      sha256 = "sha256-UKwjXJ5CyUoGpiNkyYl/2sg7E3Iw8Lsv5/1IJkdbvAo=";
+    };
+    inherit version;
+    modDirVersion = version;
+    defconfig = "pinenote_defconfig";
+    ignoreConfigErrors = true;
+    structuredExtraConfig =
+      with lib.kernel;
+      {
+        VIDEO_THP7312 = no;
+        CRYPTO_AEGIS128_SIMD = no;
+        ROCKCHIP_DW_HDMI_QP = lib.mkForce no;
+      }
+      // structuredExtraConfig;
+  }
+  // argsOverride
+)).overrideAttrs
   (oldAttrs: {
     postInstall = ''
       cp "$out/dtbs/rockchip/rk3566-pinenote-v1.2.dtb" "$out/dtbs/rockchip/pn.dtb"
